4759aad5b4e7c8815e928aaee63bc11944131cf4
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-01-09 00:47:10 +0530

    Backend Test

--- frontend/src/components/ui/InputBox.tsx
+++ frontend/src/components/ui/InputBox.tsx
@@ -1,12 +1,13 @@
 interface inputProp {
     placeholder:string,
     type:string,
-    required:boolean
+    required:boolean,
+    reference?:any
 }
 
 const InputBox = (props:inputProp) => {
   return (
-    <input required={props.required} className="w-[90%] border py-2 outline-none shadow-lg px-3 rounded-md " type={props.type} placeholder={props.placeholder}/>
+    <input required={props.required} ref={props.reference}  className="w-[90%] border py-2 outline-none shadow-lg px-3 rounded-md " type={props.type} placeholder={props.placeholder}/>
   )
 }
 
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,17 +1,59 @@
 import InputBox from "../components/ui/InputBox";
 import Button from "../components/ui/Button";
 import Navbar from "../components/ui/Navbar";
+import { useRef } from "react";
+import axios from "axios";
 
 const SignUp = () => {
+  const usernameRef = useRef<HTMLInputElement>();
+  const emailRef = useRef<HTMLInputElement>();
+  const passwordRef = useRef<HTMLInputElement>();
+
+const OnButtonClick = () => {
+
+  const username = usernameRef.current?.value;
+  const email = emailRef.current?.value;
+  const password = passwordRef.current?.value;
+
+  // Backend not yet deployed so localhost xD (suggest some website for free deployment of node backend)
+  const response = axios.post(`http://localhost:3000/api/v1/signup`, {
+    username,
+    email,
+    password,
+  });
+
+  console.log(response);
+};
+  
   return (
     <div className="pt-5 bg-secondary">
       <Navbar />
       <div className=" flex justify-center items-center min-w-screen min-h-screen p-4">
         <div className="w-[36rem] h-[36rem] flex flex-col gap-4 items-center justify-center rounded-lg bg-slate-300 bg-opacity-50 shadow ">
-          <InputBox placeholder="username" type="text" required={true}/>
-          <InputBox placeholder="email" type="email" required={true}/>
-          <InputBox placeholder="password" type="password" required={true}/>
-          <Button  text="Sign Up" size="md" variant="primary" />
+          <InputBox
+            reference={usernameRef}
+            placeholder="username"
+            type="text"
+            required={true}
+          />
+          <InputBox
+            reference={ emailRef }
+            placeholder="email"
+            type="email"
+            required={true}
+          />
+          <InputBox
+            reference={passwordRef}
+            placeholder="password"
+            type="password"
+            required={true}
+          />
+          <Button
+            text="Sign Up"
+            size="md"
+            variant="primary"
+            OnClickFn={OnButtonClick}
+          />
         </div>
       </div>
     </div>
