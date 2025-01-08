a68f26a21cde9c79d07ec0f8eea7c666addf1de4
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-01-09 00:56:36 +0530

    configured cors domain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,7 @@ declare global {
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: ["http://localhost:5173","https://brainlybybeast.vercel.app/"], 
   methods: ["GET", "POST", "PUT", "DELETE"], 
   credentials: true, 
 }));
